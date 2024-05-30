c70ef4021c314945c361cc1b1ffb57247f4a9364
rohitkrsoni <krrohit37@gmail.com>
2024-05-31 02:29:36 +0930

    Initial Commit with generated client

---
+++ SampleAPIClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Kiota.Abstractions.Authentication;
+using System;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiSdk;
+
+namespace SampleAPIClient
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+
+			var authProvider = new AnonymousAuthenticationProvider();
+			var requestAdapter = new HttpClientRequestAdapter(authProvider);
+
+			var client = new GraphClient(requestAdapter);
+			var x = await client.GetAsGetResponseAsync();
+			Console.WriteLine($"Id: {x.Id}");
+			Console.WriteLine($"Name: {x.Name}");
+		}
+	}
+}
